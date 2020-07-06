c2da76226681e9c7bb733117fa3497aa2c622c28
Axel Castro <axel@prsoft.com>
2020-07-06 13:27:26 -0400

    Delete public from .gitignore

--- client/src/main.js
+++ client/src/main.js
@@ -5,7 +5,7 @@ import store from './store'
 import Bootstrap from 'bootstrap-vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
- 
+
 Vue.use(VueAxios, axios)
 Vue.use(Bootstrap)
 
@@ -15,8 +15,8 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.config.productionTip = false
 
 // API URL
-axios.defaults.baseURL = 'http://localhost:3000/api'; // Local
-// axios.defaults.baseURL = 'https://pigmentus.herokuapp.com/api'; // Production
+// axios.defaults.baseURL = 'http://localhost:3000/api'; // Local
+axios.defaults.baseURL = 'https://pigmentus.herokuapp.com/api'; // Production
 
 new Vue({
   router,
